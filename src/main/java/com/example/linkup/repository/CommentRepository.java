f08715b6f4bbb2a33bffa3bbe737d54c883bd946
minhzom <minh1382005@gmail.com>
2025-07-10 16:11:54 +0700

    sắp xếp thứ tự hiển thị comment

--- src/main/java/com/example/linkup/repository/CommentRepository.java
+++ src/main/java/com/example/linkup/repository/CommentRepository.java
@@ -2,11 +2,16 @@ package com.example.linkup.repository;
 
 import com.example.linkup.entity.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comments, Integer> {
-    List<Comments> findAllByPostId(int PostId);
+    @Query("SELECT c FROM Comments c " +
+            "WHERE c.post.id = :postId " +
+            "ORDER BY c.updatedTime DESC")
+    List<Comments> findAllByPostId(@Param("postId") int postId);
 }
